131d44ec230aa34494f4ddc06bb29f38b3a0b8ed
Sebastian Gieser <seb@gieser.se>
2025-06-06 00:31:16 +0200

    Create GservicesProvider.java

---
+++ services-framework-proxy/src/main/java/org/microg/gms/gcm/GservicesProvider.java
@@ -0,0 +1,37 @@
+public class GservicesProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // You can just return an empty cursor for now
+        MatrixCursor cursor = new MatrixCursor(new String[]{"key", "value"});
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return "vnd.android.cursor.item/vnd.google.android.gsf.gservices";
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        return 0;
+    }
+}
